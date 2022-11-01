dafb8960f5a38e72ed86cbb5f61a0d91f5983e77
birdpersson <aleksandarvujasin@gmail.com>
2022-11-01 21:21:44 +0100

    Add test data

--- src/HospitalLibrary/GraphicalEditor/Model/DTO/BuildingDTO.cs
+++ src/HospitalLibrary/GraphicalEditor/Model/DTO/BuildingDTO.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HospitalLibrary.GraphicalEditor.Model.DTO
 {
-    internal class BuildingDTO
+    public class BuildingDTO
     {
+    public int Id { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public string Color { get; set; }
+    public string Name { get; set; }
+
+    public BuildingDTO(Building building)
+    {
+        Id = building.Id;
+        X = 100;
+        Y = 100;
+        Width = 450;
+        Height = 150;
+        Color = "gray";
+        Name = building.Name;
     }
+}
 }
