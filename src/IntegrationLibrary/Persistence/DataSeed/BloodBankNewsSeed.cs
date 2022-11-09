866678d7780362d26273026e9ed94063888100ed
todorbelic <belictodor@gmail.com>
2022-11-09 14:29:16 +0100

    'blood_bank_news_model'

---
+++ src/IntegrationLibrary/Persistence/DataSeed/BloodBankNewsSeed.cs
@@ -0,0 +1,20 @@
+﻿using IntegrationLibrary.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationLibrary.Persistence.DataSeed
+{
+    public static class BloodBankNewsSeed
+    {
+        public static void SeedBloodBankNews(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BloodBankNews>().HasData(
+                new BloodBankNews() { id=1, text="text", subject="subject1", archived=false, byteArray=Array.Empty<byte>(), published=false}
+            );
+        }
+    }
+}
