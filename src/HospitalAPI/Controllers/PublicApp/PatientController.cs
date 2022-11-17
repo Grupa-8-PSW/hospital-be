302bf11cd7a6ff2656e1ac8be44a61cdcbdf226c
Darko Filipovic <darkofilipovic1010@gmail.com>
2022-11-17 01:19:15 +0100

    added controller and DTO classes

---
+++ src/HospitalAPI/Controllers/PublicApp/PatientController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HospitalAPI.DTO;
+using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAPI.Controllers.PublicApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IPatientService _patientService;
+
+        public PatientController(IPatientService patientService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _patientService = patientService;
+        }
+        
+
+        [HttpGet]
+        [Route ("{id}")]
+        public ActionResult Get(int id)
+        {
+            var patient = _patientService.GetById(id);
+            if(patient is not null)
+            {
+                return Ok(_mapper.Map<PatientDTO>(patient));
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ src/HospitalAPI/DTO/PatientDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace HospitalAPI.DTO
+{
+    public class PatientDTO
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string StreetNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string Email { get; set; }
+    
+        public int BloodType { get; set; }
+
+    }
+}
