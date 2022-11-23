8ec34af0b016909f1d5b04e9743d1c07bfb1123d
Darko Filipovic <darkofilipovic1010@gmail.com>
2022-11-23 23:53:32 +0100

    integration test

---
+++ HospitalTests/HospitalAPITests/Integration/Controllers/PublicApp/DoctorTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using HospitalAPI;
+using HospitalAPI.Controllers.InternalApp;
+using HospitalAPI.Controllers.PublicApp;
+using HospitalAPI.DTO;
+using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Service;
+using HospitalTests.HospitalAPITests.Setup;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace HospitalTests.HospitalAPITests.Integration.Controller.InternalApp
+{
+    public class DoctorTests : BaseIntegrationTest
+    {
+        public DoctorTests(TestDatabaseFactory<Startup> factory) : base(factory) { }
+
+        private static DoctorController SetupController(IServiceScope scope)
+        {
+            return new DoctorController(scope.ServiceProvider.GetRequiredService<IDoctorService>(),scope.ServiceProvider.GetRequiredService<IPatientService>(), scope.ServiceProvider.GetRequiredService<IMapper>());
+        }
+
+        [Fact]
+        public void getsAllGeneralPracticioners()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+
+            var result = ((OkObjectResult)controller.GetAllGeneralPracticioners());
+
+            result.ShouldNotBeNull();
+        }
+        [Fact]
+        public void GetsAvailableGeneralPracticioners()
+        {
+            using var scope = Factory.Services.CreateScope();
+            var controller = SetupController(scope);
+
+            var result = ((OkObjectResult)controller.GetAllAvailableGeneralPracticioners());
+
+            result.ShouldNotBeNull();
+        }
+    }
+}
