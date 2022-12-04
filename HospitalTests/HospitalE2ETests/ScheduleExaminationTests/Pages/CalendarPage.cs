95f843a53eee3f7b084d04b7f60310e2ba608866
Damjan <dimitrijevicdamjandd@gmail.com>
2022-12-04 11:50:46 +0100

    E2E for examination scheduling

---
+++ HospitalTests/HospitalE2ETests/ScheduleExaminationTests/Pages/CalendarPage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+
+namespace HospitalTests.HospitalE2ETests.ScheduleExaminationTests.Pages;
+
+public class CalendarPage
+{
+    private readonly IWebDriver _driver;
+    public const string URI = "http://localhost:4200/calendar";
+
+    private IWebElement Calendar => _driver.FindElement(By.ClassName("mat-calendar"));
+    private IWebElement ChooseButton => _driver.FindElement(By.CssSelector("button[type='submit']"));
+
+    private IWebElement CalendarDateButton => _driver.FindElement(
+        By.XPath("//button[@aria-label='2022-12-16T00:00:00+01:00']"));
+
+    public CalendarPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    #region Ensure Displayed
+
+    public bool CalendarDisplayed()
+    {
+        return Calendar.Displayed;
+    }
+
+    public bool ChooseButtonDisplayed()
+    {
+        return ChooseButton.Displayed;
+    }
+
+    #endregion
+
+    public void InsertDate(string date)
+    {
+        Calendar.SendKeys(date);
+    }
+
+    public void SelectDate()
+    {
+        CalendarDateButton.Click();
+    }
+
+    public void ChooseDate()
+    {
+        ChooseButton.Click();
+    }
+}
---
+++ HospitalTests/HospitalE2ETests/ScheduleExaminationTests/Pages/ExaminationsPage.cs
@@ -0,0 +1,85 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HospitalTests.HospitalE2ETests.ScheduleExaminationTests.Pages;
+
+public class ExaminationsPage
+{
+    private readonly IWebDriver _driver;
+
+    public const string URI = "http://localhost:4200/examinations/16/12/2022";
+
+    private IWebElement Table => _driver.FindElement(By.Id("examinationsTable"));
+
+    //private IReadOnlyCollection<IWebElement> Rows =>
+    //    _driver.FindElements(By.XPath("//table[@id=examinationsTable]/tbody/tr"));
+
+    private IReadOnlyCollection<IWebElement> Rows =>
+        _driver.FindElements(By.CssSelector("table[id='examinationsTable'] tbody tr"));
+
+    private IWebElement LastRowStartTime =>
+        _driver.FindElement(By.XPath("//table[@id='examinationsTable']/tbody/tr[last()]/td[1]"));
+    private IWebElement LastRowEndTime =>
+        _driver.FindElement(By.XPath("//table[@id='examinationsTable']/tbody/tr[last()]/td[2]"));
+    private IWebElement LastRowPatientId =>
+        _driver.FindElement(By.XPath("//table[@id='examinationsTable']/tbody/tr[last()]/td[3]"));
+
+    private IWebElement ScheduleExaminationButton =>
+        _driver.FindElement(By.Id("scheduleExamination"));
+
+    public ExaminationsPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+    public void EnsurePageIsDisplayed()
+    {
+        var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 20));
+        wait.Until(condition =>
+        {
+            try
+            {
+                return Rows.Count > 0;
+            }
+            catch (StaleElementReferenceException)
+            {
+                return false;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        });
+    }
+
+    public bool ButtonDisplayed()
+    {
+        return ScheduleExaminationButton.Displayed;
+    }
+
+    public void ClickButton()
+    {
+        ScheduleExaminationButton.Click();
+    }
+
+    public int GetRowsCount()
+    {
+        return Rows.Count;
+    }
+
+    public string GetLastRowStartTime()
+    {
+        return LastRowStartTime.Text;
+    }
+
+    public string GetLastRowEndTime()
+    {
+        return LastRowEndTime.Text;
+    }
+    public string GetLastRowPatientId()
+    {
+        return LastRowPatientId.Text;
+    }
+
+    public void Navigate() => _driver.Navigate().GoToUrl(URI);
+
+}
