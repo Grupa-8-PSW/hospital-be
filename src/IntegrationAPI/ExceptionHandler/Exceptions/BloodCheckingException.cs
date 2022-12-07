2bcbd77a00a540756eeb524fdf1b4cfdf6409d20
todorbelic <belictodor@gmail.com>
2022-12-07 23:34:39 +0100

    selenium tests for checking for blood amount

---
+++ src/IntegrationAPI/ExceptionHandler/Exceptions/BloodCheckingException.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+
+namespace IntegrationAPI.ExceptionHandler.Exceptions
+{
+    public class BloodCheckingException : Exception
+    {
+        public BloodCheckingException()
+        {
+
+        }
+
+        public BloodCheckingException(string message)
+            : base(message)
+        {
+
+        }
+
+        public BloodCheckingException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+
+        public BloodCheckingException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+    }
+}
