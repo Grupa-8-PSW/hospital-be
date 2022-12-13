100b3a8fe5163180770316029a98eb5b4f208c59
unknown <dzonik125@gmail.com>
2022-12-13 12:01:30 +0100

    Fixed e2e tests and implemented monthlySubscription repo

--- src/IntegrationLibrary/Core/Model/MonthlySubscription.cs
+++ src/IntegrationLibrary/Core/Model/MonthlySubscription.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,22 @@ namespace IntegrationLibrary.Core.Model
         public List<Blood> RequestedBlood { get; set; }
         public DateTime DeliveryDate { get; set; }
         public BloodBank Bank { get; set; }
+        public int BankId { get; set; }
         public SubscriptionStatus Status { get; set; }
+
+        public MonthlySubscription(List<Blood> requestedBlood, DateTime deliveryDate, BloodBank bank, int bankId, SubscriptionStatus status)
+        {
+            RequestedBlood = requestedBlood;
+            DeliveryDate = deliveryDate;
+            Bank = bank;
+            BankId = bankId;
+            Status = status;
+        }
+
+        public MonthlySubscription()
+        {
+                
+        }
+        
     }
 }
