35cefd5b94c8b11e8cec223720952ace313923bc
JovanovicDev <jovanovicdusan59@yahoo.com>
2022-12-21 17:58:39 +0100

    implemented aggregate layout and initial design

---
+++ src/HospitalLibrary/Core/Model/Aggregates/AppointmentScheduling/Events/DateTimeSelected.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalLibrary.Core.Model.Aggregates;
+
+namespace HospitalLibrary.Core.Model.Aggregates.AppointmentScheduling.Events
+{
+    public class DateSelected : DomainEvent
+    {
+        public DateSelected(int id, DateTime date) : base(id)
+        {
+            Date = date;
+        }
+
+        public DateTime Date { get; private set; }
+
+    }
+}
---
+++ src/HospitalLibrary/Core/Model/Aggregates/DomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Model.Aggregates
+{
+    public abstract class DomainEvent
+    {
+        public DomainEvent(int aggregateId)
+        {
+            Id = aggregateId;
+        }
+        public virtual int Id { get; set; }
+    }
+}
