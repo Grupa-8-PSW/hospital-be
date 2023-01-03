3005c4692a80cbb0ba725fd9514a55bb820fcc02
birdpersson <aleksandarvujasin@gmail.com>
2023-01-03 15:57:30 +0100

    Initial renovation sourcing

---
+++ src/HospitalLibrary/Core/Model/Aggregates/RenovationScheduling/Events/RenovationScheduled.cs
@@ -0,0 +1,15 @@
+﻿using HospitalLibrary.Core.Model.ValueObjects;
+
+namespace HospitalLibrary.Core.Model.Aggregates.RenovationScheduling.Events
+{
+    public class RenovationScheduled : DomainEvent
+    {
+        public RenovationScheduled(int aggregateId, DateRange dateRange)
+            : base(aggregateId)
+        {
+            DateRange = dateRange;
+        }
+
+        public DateRange DateRange { get; private set; }
+    }
+}
---
+++ src/HospitalLibrary/Core/Model/Aggregates/RenovationScheduling/RenovationSchedulingSessionSnapshot.cs
@@ -0,0 +1,7 @@
+﻿namespace HospitalLibrary.Core.Model.Aggregates.RenovationScheduling
+{
+    public class RenovationSchedulingSessionSnapshot
+    {
+        public int Version { get; set; }
+    }
+}
